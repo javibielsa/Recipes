fceb30734e8f9435c84d82bcc44a9c5d4ba527a5
JoTerrance <gorka.zgz@gmail.com>
2018-01-24 09:32:25 +0100

    now the form data are saved into a inmemory struct recipe

--- src/main/java/es/salesianos/controller/IndexController.java
+++ src/main/java/es/salesianos/controller/IndexController.java
@@ -1,11 +1,9 @@
 package es.salesianos.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,80 +12,66 @@ import es.salesianos.model.CookingTool;
 import es.salesianos.model.Ingredient;
 import es.salesianos.model.Recipe;
 
-
 @Controller
 public class IndexController {
 
 	private static Logger log = LogManager.getLogger(IndexController.class);
 
-	private List<CookingTool> cookingTools = new ArrayList<CookingTool>();
-	private List<Ingredient> ingredients = new ArrayList<Ingredient>();
-	
+	private Recipe recipe;
+
 	@GetMapping("/")
 	public ModelAndView index() {
-		cookingTools.clear();
-		ingredients.clear();
-		Recipe recipe = new Recipe();
+		recipe = new Recipe();
 		ModelAndView modelAndView = new ModelAndView("index", "command", recipe);
-		modelAndView.addObject("recipe", recipe);
+		modelAndView.addObject("recipe", this.recipe);
 		return modelAndView;
 	}
 
 	@PostMapping("recipeInsert")
 	public ModelAndView recipeInsert(Recipe recipe) {
-		log.debug("recipeInsert:" + recipe.toString());
+		log.debug("recipeInsert:" + this.recipe.toString());
 		ModelAndView modelAndView = new ModelAndView("index", "command", recipe);
-		addPageData(recipe, modelAndView);
+		addPageData(recipe);
+		modelAndView.addObject("recipe", this.recipe);
 		return modelAndView;
 	}
 
-	private void addPageData(Recipe recipe, ModelAndView modelAndView) {
-		modelAndView.addObject("recipe", recipe);
-		modelAndView.addObject("name", recipe.getName());
-		modelAndView.addObject("ingredients", getIngredients());
-		modelAndView.addObject("cookingTools", getCookingTools());
+	private void addPageData(Recipe recipeForm) {
+
+		if (!StringUtils.isEmpty(recipeForm.getName())) {
+			this.recipe.setName(recipeForm.getName());
+		}
+
+		if (!StringUtils.isEmpty(recipeForm.getCookingToolName())) {
+			CookingTool cookingTool = new CookingTool();
+			cookingTool.setName(recipeForm.getCookingToolName());
+			recipeForm.setCookingToolName("");
+			this.recipe.getTools().add(cookingTool);
+		}
+		if (!StringUtils.isEmpty(recipeForm.getIngredientName())) {
+			Ingredient ingredient = new Ingredient();
+			ingredient.setName(recipeForm.getIngredientName());
+			recipeForm.setIngredientName("");
+			this.recipe.getIngredients().add(ingredient);
+		}
 	}
 
 	@PostMapping("ingredientInsert")
 	public ModelAndView ingredientInsert(Recipe recipe) {
-		log.debug("ingredientInsert:" + recipe.toString());
-
-		Ingredient ingredient = new Ingredient();
-		ingredient.setName(recipe.getIngredientName());
-		getIngredients().add(ingredient);
-
+		log.debug("ingredientInsert:" + this.recipe.toString());
+		addPageData(recipe);
 		ModelAndView modelAndView = new ModelAndView("index", "command", recipe);
-		addPageData(recipe, modelAndView);
+		modelAndView.addObject("recipe", this.recipe);
 		return modelAndView;
 	}
 
 	@PostMapping("cookingToolInsert")
 	public ModelAndView cookingToolInsert(Recipe recipe) {
-		log.debug("cookingToolInsert:" + recipe.toString());
-
-		CookingTool cookingTool = new CookingTool();
-		cookingTool.setName(recipe.getCookingToolName());
-		getCookingTools().add(cookingTool);
-
+		log.debug("cookingToolInsert:" + this.recipe.toString());
+		addPageData(recipe);
 		ModelAndView modelAndView = new ModelAndView("index", "command", recipe);
-		addPageData(recipe, modelAndView);
+		modelAndView.addObject("recipe", this.recipe);
 		return modelAndView;
-	}
-
-	public List<CookingTool> getCookingTools() {
-		return cookingTools;
-	}
-
-	public void setCookingTools(List<CookingTool> cookingTools) {
-		this.cookingTools = cookingTools;
-	}
-
-	public List<Ingredient> getIngredients() {
-		return ingredients;
-	}
-
-	public void setIngredients(List<Ingredient> ingredients) {
-		this.ingredients = ingredients;
 	}
 
 }
